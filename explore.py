a8bced1f569fb22a0b5ca952b7c94c8605f420cc
Yvonne K <yvonneking017@gmail.com>
2020-05-08 08:52:36 -0500

    added distro visuals

--- explore.py
+++ explore.py
@@ -230,6 +230,65 @@ def t_test_location_cluster_k6(train):
     print(f"Our p value for Cluster 5 is {p5:.2}")
     return p0, p1, p2, p3, p4, p5
 
+
+def t_test_year_sqft_roomcount(train):
+    """Runs a T-Test for each cluster group overall logerror mean
+    alpha is set to a 95% confidence
+    returns a p value for each cluster"""
+    alpha = .05
+    
+    loc_0 = train[train.n_size_and_year_cluster == 0]
+    t0, p0 = stats.ttest_1samp(loc_0.logerror, train.logerror.mean())
+    if p0 < alpha:
+        print("We reject the null hypothesis for Cluster 0")
+    else:
+        print("We fail to reject the null hypothesis for Cluster 0")
+    
+    loc_1 = train[train.n_size_and_year_cluster == 1]
+    t1, p1 = stats.ttest_1samp(loc_1.logerror, train.logerror.mean())
+    if p1 < alpha:
+        print("We reject the null hypothesis for Cluster 1")
+    else:
+        print("We fail to reject the null hypothesis for Cluster 1")
+        
+    loc_2 = train[train.n_size_and_year_cluster == 2]
+    t2, p2 = stats.ttest_1samp(loc_2.logerror, train.logerror.mean())
+    if p2 < alpha:
+        print("We reject the null hypothesis for Cluster 2")
+    else:
+        print("We fail to reject the null hypothesis for Cluster 2")   
+        
+    loc_3 = train[train.n_size_and_year_cluster == 3]
+    t3, p3 = stats.ttest_1samp(loc_3.logerror, train.logerror.mean())
+    if p3 < alpha:
+        print("We reject the null hypothesis for Cluster 3")
+    else:
+        print("We fail to reject the null hypothesis for Cluster 3")
+        
+    loc_4 = train[train.n_size_and_year_cluster == 4]
+    t4, p4 = stats.ttest_1samp(loc_4.logerror, train.logerror.mean())
+    if p3 < alpha:
+        print("We reject the null hypothesis for Cluster 4")
+    else:
+        print("We fail to reject the null hypothesis for Cluster 4")
+        
+    loc_5 = train[train.n_size_and_year_cluster == 5]
+    t5, p5 = stats.ttest_1samp(loc_5.logerror, train.logerror.mean())
+    if p5 < alpha:
+        print("We reject the null hypothesis for Cluster 5")
+    else:
+        print("We fail to reject the null hypothesis for Cluster 5")
+        
+    print(f"Our p value for Cluster 0 is {p0:.2}")
+    print(f"Our p value for Cluster 1 is {p1:.2}")
+    print(f"Our p value for Cluster 2 is {p2:.2}")
+    print(f"Our p value for Cluster 3 is {p3:.2}")
+    print(f"Our p value for Cluster 4 is {p4:.2}")
+    print(f"Our p value for Cluster 5 is {p5:.2}")
+    return p0, p1, p2, p3, p4, p5
+
+
+
 ## Functions for visuals
 
 
@@ -249,3 +308,63 @@ def boxplots_for_logerror_clusters(train):
     plt.title("Assessed Home Value")
     sns.boxplot(data=train, x="taxvaluedollarcnt", y="s_cluster_target")
     plt.suptitle("Logerror Clusters against Various Features")
+
+    
+def logerror_distro_visualization(df):
+    """
+    This visualization shows you 4 distrubution plots of the logerrors
+    1st = all counties
+    2nd = LA county
+    3rd = Orange County
+    4th = Ventura County
+    """
+    c="#006AFF"
+    zillow = df
+    la_county = df[df.County == "Los Angeles"]
+    orange_county = df[df.County == "Orange"]
+    ventura_county = df[df.County == "Ventura"]
+    
+    plt.figure(figsize=(12,12))
+    sns.distplot(zillow.logerror, color=c)
+    plt.xlim(-1, 1)
+    plt.ylabel("Count")
+    plt.title("Logerror Distribution for all three Counties")
+
+    plt.figure(figsize=(12,12))
+
+    plt.suptitle('Logerror Distribution by County - note Y axis is not on the same scale', fontsize=14)
+
+    plt.subplot(3, 3, 1)
+    sns.distplot(la_county.logerror, color=c)
+    plt.xlim(-.25, .25)
+    plt.ylabel("Count")
+    plt.title("Los Angeles")
+
+    plt.subplot(3, 3, 2)
+    sns.distplot(orange_county.logerror, color=c)
+    plt.xlim(-.25, .25)
+    plt.ylabel("Count")
+    plt.title("Orange County")
+
+    plt.subplot(3, 3, 3)
+    sns.distplot(ventura_county.logerror, color=c)
+    plt.xlim(-.25, .25)
+    plt.ylabel("Count")
+    plt.title("Ventura County")
+    
+def num_home_by_county(train):
+    """
+    Returns a graph with the number of homes in each county
+    """
+
+    plt.rcParams["figure.figsize"] = (13,10)
+
+    ax = sns.countplot(data=train, x='County', palette='Blues_r', order=['Los Angeles', 'Orange', 'Ventura'])
+
+    for p in ax.patches:
+            ax.annotate('{:.0f}'.format(p.get_height()), (p.get_x()+0.3, p.get_height()+50))
+
+    plt.title('Number of Homes by County (train dataset)')
+    plt.xlabel('County Name')
+    plt.ylabel('Number of Homes')
+    plt.show()
